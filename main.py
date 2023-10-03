f5a66c8041a41e1bd93ac87a9036f124e2fccf79
MinhhQuangg <minhquang030120@gmail.com>
2023-10-03 11:58:50 -0500

    3/10

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def tokenize_into_2_grams(token_list):
     list_2_grams = []
 
     for i in range(len(token_list)-1):
-        list_2_grams.append(token_list[i : i + 2])
+        list_2_grams.append(token_list[i : i + 3])
 
     return list_2_grams
 
@@ -31,8 +31,6 @@ def tokenize_into_2_grams(token_list):
 target_2_grams = tokenize_into_2_grams(token_list_target)
 #ngrams the list of tokenize corpus
 corpus_2_grams = tokenize_into_2_grams(token_list_corpus)
-
-print(target_2_grams)
 
 #compare the to find the redundant
 repeat = 0
@@ -42,6 +40,33 @@ for list_1 in target_2_grams:
             repeat +=1
             break
 
-redundant = repeat / len(target_2_grams)
+redundant = (repeat+1) / len(target_2_grams)
 print(redundant)
 
+
+def tokenize_into_3_grams(token_list):
+    list_3_grams = []
+
+    for i in range(len(token_list)-2):
+        list_3_grams.append(token_list[i : i + 3])
+
+    return list_3_grams
+
+#ngrams the list of tokenize target project
+target_3_grams = tokenize_into_3_grams(token_list_target)
+#ngrams the list of tokenize corpus
+corpus_3_grams = tokenize_into_3_grams(token_list_corpus)
+
+print(target_3_grams)
+print(corpus_3_grams)
+
+#compare the to find the redundant
+repeat = 0
+for list_1 in target_3_grams:
+    for list_2 in corpus_3_grams:
+        if ( list_1 == list_2) :
+            repeat +=1
+            break
+
+redundant = (repeat+2) / len(target_3_grams)
+print(redundant)
